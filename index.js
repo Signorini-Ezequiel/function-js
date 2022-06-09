2ecdb999bad79e15dd3f6beeb8b170ad642d67c2
Ezequiel <ezequielsignorini@iresm.edu.ar>
2022-06-09 09:46:06 -0300

    add files

---
+++ index.js
@@ -0,0 +1,20 @@
+let resta = (a=2, b=1) => {console.log(`El resultado de: ${a}-${b} es: ${a-b}`);};
+
+resta();
+resta(10, 3);
+
+let posne = (num="none") => {
+    if (num >= 0) {
+        console.log("El número es positivo");
+    }
+    else if (num <= 0) {
+        console.log("El número es negativo");
+    }
+    else {
+        console.log("Debe ingresar un número");
+    }
+};
+
+posne();
+posne(123);
+posne(-12);
